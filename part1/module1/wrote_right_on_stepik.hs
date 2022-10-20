c3cfa4daa3877d720649653b6ab9f2233952cf56
vova <sevlabr@yandex.ru>
2022-10-20 17:05:13 +0300

    add types lesson

--- part1/module1/wrote_right_on_stepik.hs
+++ part1/module1/wrote_right_on_stepik.hs
@@ -6,3 +6,10 @@ sign x = if x > 0 then 1 else (if x < 0 then (-1) else 0)
 x |-| y = abs(x - y)
 
 logBase 4 $ min 20 $ 9 + 7 -- logBase 4 (min 20 (9 + 7))
+
+import Data.Char
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int x y = if isDigit(x) && isDigit(y) then 10 * digitToInt(x) + digitToInt(y) else 100
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist p1 p2 = sqrt ((fst p1 - fst p2) ^ 2 + (snd p1 - snd p2) ^ 2)
