83b4154c881b01db61f71601756f5589af1aa9bb
vova <sevlabr@yandex.ru>
2022-11-17 12:06:37 +0300

    add area calc

--- part1/module4/geometry.hs
+++ part1/module4/geometry.hs
@@ -14,3 +14,10 @@ distance (Point x1 y1) (Point x2 y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 
 -- *Demo> distance (Point 3.1 4.5) (Point 0.6 (-0.9))
 -- 5.950630218724736
+
+
+data Shape = Circle Double | Rectangle Double Double
+
+area :: Shape -> Double
+area (Circle    r)   = pi * r^2
+area (Rectangle a b) = a * b
